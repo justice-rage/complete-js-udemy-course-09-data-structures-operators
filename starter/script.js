1d4e2bfca73d719b8bb00774595937444b92e67a
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-15 10:49:16 -0700

    complete lecture 103

--- starter/script.js
+++ starter/script.js
@@ -4,6 +4,7 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+// Lecture 103: Destructuring Arrays
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,6 +12,10 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +32,40 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[0];
+const c = arr[0];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// receive 2 return values from a fucntion
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
+const [p=1, q=1, r=1] = [8];
+console.log(p, q, r);
