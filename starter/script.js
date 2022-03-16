055e34ea8e0542898f5577e4a2c33251bc584c48
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-15 18:04:58 -0700

    complete lecture 109

--- starter/script.js
+++ starter/script.js
@@ -47,6 +47,39 @@ const restaurant = {
   },
 };
 
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// Lecture 109: Logical Assignment Operators
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// nullish assignment operator (null or undefined)
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest2.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
+
 // Lecture 108: The Nullish Coalescing Operator (??)
 
 // restaurant.numGuests = 0;
