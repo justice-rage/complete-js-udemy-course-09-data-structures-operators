91162fe8114cec21e9f7ca55181577ecc789020a
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-22 18:02:29 -0700

    WIP coding challenge #1

--- starter/script.js
+++ starter/script.js
@@ -5,9 +5,9 @@
 
 /*
 1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For BayernMunic (team 1) create one variable ('gk') with goalkeeper's name, and one array ('fieldPlayers') with all th remaining 10 field players.
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players.
 3. Create an array 'allPlayers' containing all players of both teams (22 players).
-4. during the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original tem1 players plus 'Thiago', 'Coutinho' and 'Perisic'.
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'.
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2').
 6. Write a function ('printGoals') that receives an arbitrary number of players names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of players names passed in).
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
@@ -55,6 +55,20 @@ const game = {
     team2: 6.5,
   }
 }
+
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3. 
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4. 
 //////////////////////////////////////////////
 
 /*
