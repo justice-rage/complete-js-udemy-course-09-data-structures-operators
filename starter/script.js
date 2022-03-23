4bb6a2668fe270c0a80fabd04a45b38b12e2e053
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-22 17:35:17 -0700

    add challenge #1 starter code

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,63 @@
 'use strict';
 
+////////////////////////////////////////////////
+// Coding Challenge #1
+
+/*
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For BayernMunic (team 1) create one variable ('gk') with goalkeeper's name, and one array ('fieldPlayers') with all th remaining 10 field players.
+3. Create an array 'allPlayers' containing all players of both teams (22 players).
+4. during the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original tem1 players plus 'Thiago', 'Coutinho' and 'Perisic'.
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2').
+6. Write a function ('printGoals') that receives an arbitrary number of players names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of players names passed in).
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  }
+}
+//////////////////////////////////////////////
+
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
