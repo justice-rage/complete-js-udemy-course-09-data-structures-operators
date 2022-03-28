83d18a15eb4d9f23ba60bb0c4bd10d3af0b7ff79
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-28 12:55:29 -0700

    complete lecture 114

--- starter/script.js
+++ starter/script.js
@@ -45,8 +45,37 @@ const restaurant = {
   },
 };
 
+// Lecture 114: Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for(const [day, {open, close}] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+//////////////////////////////
+
 // Lecture 113: Optional Chaining (?.)
 
+/*
 if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
 // console.log(restaurant.openingHours.mon.open);
@@ -81,6 +110,7 @@ console.log(users[0]?.name ?? 'User array empty');
 
 if(users.length > 0) console.log(users[0].name);
 else console.log('user array empty')
+*/
 
 /////////////////////////////////////
 
